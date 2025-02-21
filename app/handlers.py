4db15abffefd2f257605b99e9ac45d885031b6f3
admin <alexandrpeks@gmail.com>
2025-02-21 22:21:06 +0400

    добавил клавиатуры

--- app/handlers.py
+++ app/handlers.py
@@ -2,52 +2,61 @@ import asyncio
 
 from aiogram import Router, F, Bot
 from aiogram.filters import CommandStart, Command
-from aiogram.types import Message, ReplyKeyboardRemove
+from aiogram.types import Message, ReplyKeyboardRemove, CallbackQuery
 from aiogram.enums import ChatAction
 
 import app.keyboards as kb
 
-
 router = Router()
+
 
 @router.message(CommandStart())
 async def cmd_start(message: Message):
-    await message.bot.send_chat_action(chat_id=message.from_user.id, action=ChatAction.TYPING)
-    await asyncio.sleep(0.5)
-    await message.answer('Ты нажал /start', reply_markup=ReplyKeyboardRemove())
-
-
-@router.message(Command('hello'))
-async def cmd_hello(message: Message):
-    await message.answer(f'Привет! {message.from_user.first_name}', reply_markup=kb.inline_main)
-
-
+    await message.bot.send_chat_action(chat_id=message.from_user.id,
+                                       action=ChatAction.TYPING)
+    await asyncio.sleep(0.2)
+    await message.answer(f'Привет! {message.from_user.first_name}',
+                         reply_markup=kb.inline_main)
 
 
 @router.message(Command('adminlist'))
 async def adminlist(message: Message):
     admins = await message.bot.get_chat_administrators(chat_id=-100)
-    admin_list = '\n'.join([f'Полное имя 👤 - {admin.user.full_name}, username - @{admin.user.username}' for admin in admins])
+    admin_list = '\n'.join([f'Полное имя 👤 - {admin.user.full_name}, '
+                            f'username - @{admin.user.username}'
+                            for admin in admins])
     await message.answer(f'Администраторы чата:\n{admin_list}')
-
-
 
 
 @router.message(Command("pin"))
 async def pin_custom_message(message: Message, bot: Bot):
-    chat_id = -100  # Юзернейм или ID канала
+    chat_id = -100  # @username или ID канала
     # Получаем текст после команды "/pin"
     command_parts = message.text.split(maxsplit=1)
     if len(command_parts) < 2:
-        await message.answer("❌ Ошибка! Напишите текст после /pin.\nПример: `/pin Важное сообщение`")
+        await message.answer(
+            "❌ Ошибка! Напишите текст после /pin.\nПример: `/pin Важное сообщение`")
         return
-    pin_text = command_parts[1] # Берем текст после /pin
+    pin_text = command_parts[1]  # Берем текст после /pin
     # Бот отправляет сообщение с этим текстом
     sent_message = await bot.send_message(chat_id=chat_id, text=pin_text)
     # Бот закрепляет это сообщение
-    await bot.pin_chat_message(chat_id=chat_id, message_id=sent_message.message_id)
+    await bot.pin_chat_message(chat_id=chat_id,
+                               message_id=sent_message.message_id)
 
     await message.answer("✅ Сообщение закреплено!")
 
 
+@router.callback_query(F.data == 'catalog')
+async def callback_catalog(callback: CallbackQuery):
+    await callback.answer('Каталог товаров')
+    await callback.message.edit_text('Выберите категорию',
+                                     reply_markup=await kb.builder_catalog())
 
+
+@router.callback_query(F.data.startswith('item_'))
+async def item_handler(callback: CallbackQuery):
+    brand_name = callback.data.split('_')[-1].title()
+    await callback.answer('Вы выбрали товар')
+    await callback.message.edit_text(f'Вы выбрали {brand_name}',
+                                     reply_markup=kb.back)
--- app/keyboards.py
+++ app/keyboards.py
@@ -1,4 +1,5 @@
 from aiogram.types import (ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton)
+from aiogram.utils.keyboard import InlineKeyboardBuilder
 
 main = ReplyKeyboardMarkup(keyboard=[
     [KeyboardButton(text='Каталог'),
@@ -10,8 +11,32 @@ main = ReplyKeyboardMarkup(keyboard=[
 )
 
 inline_main = InlineKeyboardMarkup(inline_keyboard=[
-    [InlineKeyboardButton(text='Каталог', url='https://youtube.com'),
-     InlineKeyboardButton(text='Корзина', url='https://youtube.com')],
-    [InlineKeyboardButton(text='Контакты', url='https://youtube.com')]
+    [InlineKeyboardButton(text='Каталог', callback_data='catalog'),
+     InlineKeyboardButton(text='Корзина', callback_data='cart')],
+    [InlineKeyboardButton(text='Контакты', callback_data='contacts')],
     ]
 )
+
+
+back = InlineKeyboardMarkup(inline_keyboard=[
+    [InlineKeyboardButton(text='Назад', callback_data='catalog'),]
+
+])
+
+cart = InlineKeyboardMarkup(inline_keyboard=[
+    [InlineKeyboardButton(text='Добавить в корзину',
+                          callback_data='add_to_cart'),
+     InlineKeyboardButton(text='Перейти к оплате',
+                          callback_data='checkout')],
+    [InlineKeyboardButton(text='Очистить корзину',
+                          callback_data='clear_cart'),]
+])
+
+
+async def builder_catalog():
+    brands = ['Adidas', 'Nike', 'Puma', 'New Balance']
+    keyboard = InlineKeyboardBuilder()
+    for brand in brands:
+        keyboard.add(InlineKeyboardButton(text=brand,
+                                          callback_data=f'item_{brand}'))
+    return keyboard.adjust(2).as_markup()
